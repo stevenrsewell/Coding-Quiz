5e35c910a31e9313c0cf7ad557dbd94901f2421d
Steven Sewell <stevenrsewell@outlook.com>
2019-12-18 21:56:04 -0600

    Questions
    Addition of the 5 questions and multiple choice answers in the questions.js file

---
+++ questions.js
@@ -0,0 +1,35 @@
+var questions = [
+
+    {
+        title: "What does CSS stand for?",
+        choices: ["Computer Style Sheets", "Colorful Style Sheets", "Creative Style Sheets", "Cascading Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+
+    {
+        title: "Where in an HTML document is the correct place to refer to an external style sheet?",
+        choices: ["<head>", "<body>", "<h1>", "At the end of the document"],
+        answer: "<head>"
+    },
+
+    {
+        title: "What is the default value of the position property in CSS?",
+        choices: ["Relative", "Fixed", "Absolute", "Static"],
+        answer: "Static"
+    },
+
+    {
+        title:
+            "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<javascript>", "<js>", "<scripting>"],
+        answer: "<script>"
+    },
+
+    {
+        title:
+            "Which event occurs when the user clicks on an HTML element?",
+        choices: ["onchange", "onclick", "onmouseover", "onmouseclick"],
+        answer: "onclick"
+    }
+
+];
