f0b20bcc3529300bb4081903bd5af3ed620d0a2d
Steven Sewell <stevenrsewell@outlook.com>
2020-03-05 23:09:15 -0600

    Updated javascript

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# Coding Quiz Homework 4
+# Coding Quiz - HW 4
+http://github.com/stevenrsewell/Coding-Quiz
+
+This application is a Javascript based quiz that a user may take to refresh their skills. The quiz begins on a button-click and is timed.
 
 ## User Story
 
 AS A coding bootcamp student ...  I WANT to take a timed quiz on JavaScript fundamentals that stores high scores ... SO THAT I can gauge my progress compared to my peers
 
 ## Dependencies Used
-HTML
-CSS
-Javascript
+<ol>
+<li>Javascript</li>
+<li>Bootstrap</li>
+</ol>
 
 ## Screenshots
-
+<img src="./Screenshot_1.png"></img>
+<br>
+<img src="./Screenshot_2.png"></img>
--- script.js
+++ script.js
@@ -1,170 +1,94 @@
-// variables to keep track of quiz state
-var currentQuestionIndex = 0;
-var time = questions.length * 15;
-var timerId;
+var quizContent = document.getElementById("question-content");
+var score = 0;
+var highscore = localStorage.getItem(".high-scores");
+var startButton = document.getElementById("start-button")
+startButton.addEventListener("click", startQuiz)
+var quizContainer = document.getElementById("quiz-container")
+var questionNum = 0
+var timer
+var quizTime = 60
+var timerEl = document.getElementById("timeClock")
 
-// variables to reference DOM elements
-var questionsEl = document.getElementById("questions");
-var timerEl = document.getElementById("time");
-var choicesEl = document.getElementById("choices");
-var submitBtn = document.getElementById("submit");
-var startBtn = document.getElementById("start");
-var initialsEl = document.getElementById("initials");
-var feedbackEl = document.getElementById("feedback");
+var questions = [
+    {
+        question: "What does CSS stand for?",
+        potential_answers: ["Computer Style Sheets", "Colorful Style Sheets", "Creative Style Sheets", "Cascading Style Sheets"],
+        correct_answer: "Cascading Style Sheets"
+    },
 
-// sound effects
-var sfxRight = new Audio("assets/sfx/correct.wav");
-var sfxWrong = new Audio("assets/sfx/incorrect.wav");
+    {
+      question: "Where in an HTML document is the correct place to refer to an external style sheet?",
+      potential_answers: ["<head>", "<body>", "<h1>", "At the end of the document"],
+      correct_answer: "<head>"
+  },
+
+  {
+      question: "What is the default value of the position property in CSS?",
+      potential_answers: ["Relative", "Fixed", "Absolute", "Static"],
+      correct_answer: "Static"
+  },
+
+  {
+      question:
+          "Inside which HTML element do we put the JavaScript?",
+      potential_answers: ["<script>", "<javascript>", "<js>", "<scripting>"],
+      answer: "<script>"
+  },
+
+  {
+      question: "Which event occurs when the user clicks on an HTML element?",
+      potential_answers: ["onchange", "onclick", "onmouseover", "onmouseclick"],
+      correct_answer: "onclick"
+  }
+]
+
 
 function startQuiz() {
-  // hide start screen
-  var startScreenEl = document.getElementById("start-screen");
-  startScreenEl.setAttribute("class", "hide");
 
-  // un-hide questions section
-  questionsEl.removeAttribute("class");
-
-  // start timer
-  timerId = setInterval(clockTick, 1000);
-
-  // show starting time
-  timerEl.textContent = time;
-
-  getQuestion();
+  renderQuestion()
+  startTimer()
 }
 
-function getQuestion() {
-  // get current question object from array
-  var currentQuestion = questions[currentQuestionIndex];
-
-  // update title with current question
-  var titleEl = document.getElementById("question-title");
-  titleEl.textContent = currentQuestion.title;
-
-  // clear out any old question choices
-  choicesEl.innerHTML = "";
-
-  // loop over choices
-  currentQuestion.choices.forEach(function(choice, i) {
-    // create new button for each choice
-    var choiceNode = document.createElement("button");
-    choiceNode.setAttribute("class", "choice");
-    choiceNode.setAttribute("value", choice);
-
-    choiceNode.textContent = i + 1 + ". " + choice;
-
-    // attach click event listener to each choice
-    choiceNode.onclick = questionClick;
-
-    // display on the page
-    choicesEl.appendChild(choiceNode);
-  });
+function startTimer() {
+  timer = setInterval(()=>{
+    quizTime --
+    timerEl.innerText = quizTime
+  },1000)
 }
 
-function questionClick() {
-  // check if user guessed wrong
-  if (this.value !== questions[currentQuestionIndex].answer) {
-    // penalize time
-    time -= 15;
+function renderQuestion() {
+  var buttons = questions[questionNum].potential_answers.map(function (answer) {
+    return `<button onclick="answerQuestion('${answer}')" >${answer}</button>`
+  })
 
-    if (time < 0) {
-      time = 0;
-    }
+  quizContainer.innerHTML = `
+    <h1>${questions[questionNum].question}</h1>
+    ${buttons.join("")}
+  `
+}
 
-    // display new time on page
-    timerEl.textContent = time;
-
-    // play "wrong" sound effect
-    sfxWrong.play();
-
-    feedbackEl.textContent = "Wrong!";
+function answerQuestion(answer) {
+  if (answer === questions[questionNum].correct_answer) {
+    score ++
   } else {
-    // play "right" sound effect
-    sfxRight.play();
-
-    feedbackEl.textContent = "Correct!";
+    score --
+    quizTime -= 10
   }
 
-  // flash right/wrong feedback on page for half a second
-  feedbackEl.setAttribute("class", "feedback");
-  setTimeout(function() {
-    feedbackEl.setAttribute("class", "feedback hide");
-  }, 1000);
-
-  // move to next question
-  currentQuestionIndex++;
-
-  // check if we've run out of questions
-  if (currentQuestionIndex === questions.length) {
-    quizEnd();
+  questionNum ++
+  console.log(questionNum)
+  console.log(score)
+  if (questionNum === questions.length) {
+    endQuiz()
   } else {
-    getQuestion();
+    renderQuestion()
   }
 }
 
-function quizEnd() {
-  // stop timer
-  clearInterval(timerId);
+function endQuiz() {
+  quizContainer.innerHTML = `
+    Your score is ${score}
+  `
+  clearInterval(timer)
 
-  // show end screen
-  var endScreenEl = document.getElementById("end-screen");
-  endScreenEl.removeAttribute("class");
-
-  // show final score
-  var finalScoreEl = document.getElementById("final-score");
-  finalScoreEl.textContent = time;
-
-  // hide questions section
-  questionsEl.setAttribute("class", "hide");
-}
-
-function clockTick() {
-  // update time
-  time--;
-  timerEl.textContent = time;
-
-  // check if user ran out of time
-  if (time <= 0) {
-    quizEnd();
-  }
-}
-
-function saveHighscore() {
-  // get value of input box
-  var initials = initialsEl.value.trim();
-
-  // make sure value wasn't empty
-  if (initials !== "") {
-    // get saved scores from localstorage, or if not any, set to empty array
-    var highscores =
-      JSON.parse(window.localStorage.getItem("highscores")) || [];
-
-    // format new score object for current user
-    var newScore = {
-      score: time,
-      initials: initials
-    };
-
-    // save to localstorage
-    highscores.push(newScore);
-    window.localStorage.setItem("highscores", JSON.stringify(highscores));
-
-    // redirect to next page
-    window.location.href = "highscores.html";
-  }
-}
-
-function checkForEnter(event) {
-  // "13" represents the enter key
-  if (event.key === "Enter") {
-    saveHighscore();
-  }
-}
-
-// user clicks button to submit initials
-submitBtn.onclick = saveHighscore;
-
-// user clicks button to start quiz
-startBtn.onclick = startQuiz;
-
-initialsEl.onkeyup = checkForEnter;
+};
